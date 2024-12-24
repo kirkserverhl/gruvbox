e9956dcc0990a532641937bebf25d7f41a47a9e4
kirkserverhl <kirkserverhl@gmail.com>
2024-12-24 17:51:55 -0500

    Update: Tue Dec 24 05:51:55 PM EST 2024

--- scripts/scripts/update_wallpaper.sh
+++ scripts/scripts/update_wallpaper.sh
@@ -3,9 +3,8 @@
 # Variables
 WALLPAPER_FILE="$HOME/.config/settings/cache/current_wallpaper"
 
-# Read the current wallpaper path
+# Read the current wallpaper path from the file
 CURRENT_WALLPAPER=$(cat "$WALLPAPER_FILE")
 
-# Set the wallpaper using Waypaper
+# Set the wallpaper using Waypaper (the full path is already in the file)
 waypaper --backend hyprpaper --wallpaper "$CURRENT_WALLPAPER"
-
