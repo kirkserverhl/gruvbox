f31f81649f86ccc1c936c09814b55207249f2423
kirkserverhl <kirkserverhl@gmail.com>
2024-12-24 17:37:25 -0500

    Update: Tue Dec 24 05:37:25 PM EST 2024

--- scripts/scripts/sync_wallpaper.sh
+++ scripts/scripts/sync_wallpaper.sh
@@ -6,10 +6,10 @@ TARGET_USER="kirk"
 TARGET_IP="192.168.0.69"
 TARGET_WALLPAPER_FILE="/home/kirk/.config/settings/cache/current_wallpaper"
 
-# Sync the wallpaper file to Computer B
+# Sync the 'current_wallpaper' file to Computer B
 scp -i ~/.ssh/id_ed25519 "$WALLPAPER_FILE" "$TARGET_USER@$TARGET_IP:$TARGET_WALLPAPER_FILE"
 
-# Optional: Trigger wallpaper change on Computer B
-ssh -i ~/.ssh/id_ed25519 "$TARGET_USER@$TARGET_IP" "waypaper set /home/kirk/wallpaper/$(cat $TARGET_WALLPAPER_FILE)"
+# Trigger wallpaper update on Computer B
+ssh -i ~/.ssh/id_ed25519 "$TARGET_USER@$TARGET_IP" "~/scripts/update_wallpaper.sh"
 
 
