6ac9c3d2cd96da25a12ac3e7e50b460db810e7d4
kirkserverhl <kirkserverhl@gmail.com>
2024-12-24 08:52:21 -0500

    Update: Tue Dec 24 08:52:21 AM EST 2024

--- assets/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c4c4c3", "#151311" },
-	[SchemeSel] = { "#c4c4c3", "#926E5F" },
-	[SchemeOut] = { "#c4c4c3", "#DAB071" },
-};
--- assets/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#151311";
-static const char* selfgcolor   = "#c4c4c3";
-static const char* normbgcolor  = "#D7724A";
-static const char* normfgcolor  = "#c4c4c3";
-static const char* urgbgcolor   = "#926E5F";
-static const char* urgfgcolor   = "#c4c4c3";
