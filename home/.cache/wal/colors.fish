8b0989ded3ed48a439a25074b25b865061405f73
kirkserverhl <kirkserverhl@gmail.com>
2025-01-14 02:30:33 -0500

    added .cache for wal

---
+++ home/.cache/wal/colors.fish
@@ -0,0 +1,25 @@
+set fish_color_normal normal
+set fish_color_command 8FC17B
+set fish_color_param 8FC17B
+set fish_color_redirection $fish_color_param
+set fish_color_comment 948B77
+set fish_color_error ff0000
+set fish_color_escape 777B72
+set fish_color_operator $fish_color_escape
+set fish_color_end 777B72
+set fish_color_quote 948B77
+set fish_color_autosuggestion 6c5959
+set fish_color_user 3E8285
+set fish_color_host 3E8285
+set fish_color_valid_path --underline
+set fish_color_cwd 3E8285
+set fish_color_cwd_root red
+set fish_color_match --background=brblue
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_cancel -r
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_completion c3c3c3 --background=0f1010
+set fish_pager_color_description $fish_color_quote yellow
+set fish_pager_color_progress 749D91 --background=0f1010
+set fish_color_history_current --bold
